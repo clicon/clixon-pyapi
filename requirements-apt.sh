bf6d58433248f76b44c72b7c05976044a6a8aa5c
Kristofer Hallin <kristofer@sunet.se>
2023-11-20 15:17:40 +0100

    Added python3-setuptools to requirements-apt.sh

--- requirements-apt.sh
+++ requirements-apt.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-sudo apt install -y python3-attr python3-daemonize python3-exceptiongroup python3-iniconfig python3-packaging python3-pluggy python3-pytest python3-tomli python3-xmltodict
+sudo apt install -y python3-attr python3-daemonize python3-exceptiongroup python3-iniconfig python3-packaging python3-pluggy python3-pytest python3-tomli python3-xmltodict python3-setuptools
