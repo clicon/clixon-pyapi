716e2559ea2cfbdbaef4a201dc82cf65174e888a
Kristofer Hallin <kristofer@sunet.se>
2024-02-01 15:04:15 +0000

    Documentation for event.py

--- clixon/event.py
+++ clixon/event.py
@@ -13,6 +13,7 @@ class RPCEventHandler():
     def __init__(self) -> None:
         """
         Initialize the event handler.
+        :return: None
         """
 
         self.events = {}
@@ -20,6 +21,8 @@ class RPCEventHandler():
     def register(self, event: str) -> None:
         """
         Register a callback to an event.
+        :param event: The event to register to.
+        :return: None
         """
 
         def decorator(callback: function) -> function:
@@ -35,6 +38,8 @@ class RPCEventHandler():
     def unregister(self, event: str, callback: function) -> None:
         """
         Unregister a callback from an event.
+        :param event: The event to unregister from.
+        :param callback: The callback to unregister.
         """
 
         if event in self.events:
@@ -47,6 +52,11 @@ class RPCEventHandler():
              **kwargs: Optional[dict]) -> None:
         """
         Emit an event.
+        :param event: The event to emit.
+        :param not_found_error: Whether to raise an error if the event fails
+        :param args: The arguments to pass to the callback.
+        :param kwargs: The keyword arguments to pass to the callback.
+        :return: None
         """
 
         for k, v in self.events.items():
