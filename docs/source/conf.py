9e8ae56ccae43f1aa8dda96631f5a47036f230d2
Kristofer Hallin <kristofer@sunet.se>
2024-02-21 14:31:18 +0100

    Config file for Sphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
 project = 'Clixon Python API'
 copyright = '2024, Kristofer Hallin'
 author = 'Kristofer Hallin'
@@ -14,19 +17,28 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ['sphinx.ext.viewcode',
+              'sphinx.ext.autodoc', "sphinx.ext.autodoc.typehints", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
+autodoc_typehints = 'description'
+html_theme = 'sphinx_rtd_theme'
 templates_path = ['_templates']
 exclude_patterns = []
-
-
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+exclude_patterns = ['args.py']
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../clixon/'))
+path = "../../"
+
+sys.path.insert(0, os.path.abspath(path))
+
+try:
+    import clixon
+except ImportError:
+    print("Clixon not installed")
+    sys.exit(1)
