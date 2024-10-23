e784eafd5d12a14932be7b9a2ab8d2980968b801
Kristofer Hallin <kristofer@sunet.se>
2024-10-23 10:34:17 +0000

    Change dependencies for Debian package.

--- setup.py
+++ setup.py
@@ -26,12 +26,8 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License"
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'pyyaml',
-        'daemonize',
-        'xmltodict'
-    ],
+    install_requires=["pyyaml", "xmltodict"],
     license="Apache-2.0",
-    python_requires='>=3.11',
+    python_requires=">=3.11",
     scripts=["clixon_server.py"],
 )
